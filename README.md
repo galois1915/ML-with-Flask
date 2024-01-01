adeda084d4a68da0f80f0b60a8a31216ca84286f
galois1915 <angel_98tcc@hotmail.com>
2024-01-01 11:04:48 -0500

    rendering a template using variables

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Simple-model-ML-with-Flask
+
+<code>export FLASK_APP=app</code>
+<code>FLASK_ENV=development</code>
+<code>flask run</code>
--- app.py
+++ app.py
@@ -1,28 +1,9 @@
-from flask import Flask, abort
-from markupsafe import escape
+from flask import Flask, render_template
+import datetime
+
 app = Flask(__name__)
 
-@app.route("/")
-@app.route("/index/")
+
+@app.route('/')
 def hello():
-    return "<h1>hello world!</h1>"
-
-@app.route("/about/")
-def about():
-    return "<h3>This is a Flask web application</h3>"
-
-@app.route("/capitalize/<word>/")
-def capitalize(word):
-    return "<h1>{}</h1>".format(escape(word.capitalize()))
-
-@app.route('/add/<int:n1>/<int:n2>/')
-def add(n1, n2):
-    return '<h1>{}</h1>'.format(n1 + n2)
-
-@app.route('/users/<int:user_id>/')
-def greet_user(user_id):
-    users = ['Bob', 'Jane', 'Adam']
-    try:
-        return '<h2>Hi {}</h2>'.format(users[user_id])
-    except:
-        abort(404)
+    return render_template('index.html', utc_dt=datetime.datetime.utcnow())
