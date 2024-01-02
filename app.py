d1c3300ce2bb8508886af4438907b3c367bf164d
galois1915 <angel_98tcc@hotmail.com>
2024-01-02 17:42:15 -0500

    show books in index.html

--- app.py
+++ app.py
@@ -1,18 +1,30 @@
 from flask import Flask, render_template
 import datetime
+import os
+import psycopg2
 
 app = Flask(__name__)
 
+def get_db_connection():
+    conn = psycopg2.connect(host='localhost',
+                            database='flask_db',
+                            user=os.environ['DB_USERNAME'],
+                            password=os.environ['DB_PASSWORD'])
+    return conn
 
 @app.route('/')
-def hello():
-    return render_template('index.html', utc_dt=datetime.datetime.utcnow())
+def index():
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute('SELECT * FROM books;')
+    books = cur.fetchall()
+    cur.close()
+    conn.close()
+    return render_template('index.html', books=books)
 
 @app.route('/about/')
 def about():
     return render_template('about.html')
-
-# ...
 
 @app.route('/comments/')
 def comments():
@@ -21,5 +33,4 @@ def comments():
                 'This is the third comment.',
                 'This is the fourth comment.'
                 ]
-
     return render_template('comments.html', comments=comments)
