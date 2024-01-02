bf7c12bafc63bcc4c85ffb0c4898cb04bf4db995
galois1915 <angel_98tcc@hotmail.com>
2024-01-02 18:34:35 -0500

    conecction with postgresql

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, redirect
 import datetime
 import os
 import psycopg2
@@ -34,3 +34,23 @@ def comments():
                 'This is the fourth comment.'
                 ]
     return render_template('comments.html', comments=comments)
+
+@app.route('/create/', methods=('GET', 'POST'))
+def create():
+    if request.method == 'POST':
+        title = request.form['title']
+        author = request.form['author']
+        pages_num = int(request.form['pages_num'])
+        review = request.form['review']
+
+        conn = get_db_connection()
+        cur = conn.cursor()
+        cur.execute('INSERT INTO books (title, author, pages_num, review)'
+                    'VALUES (%s, %s, %s, %s)',
+                    (title, author, pages_num, review))
+        conn.commit()
+        cur.close()
+        conn.close()
+        return redirect(url_for('index'))
+
+    return render_template('create.html')
