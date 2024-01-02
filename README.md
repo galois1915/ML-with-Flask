9b2f35a7a8f03661fb0998eb770a4df2a8b12d3a
galois1915 <angel_98tcc@hotmail.com>
2024-01-02 10:05:00 -0500

    if and for in jinja

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Simple-model-ML-with-Flask
 
+[jinja](https://jinja.palletsprojects.com/en/3.0.x/)
+
 <code>export FLASK_APP=app</code>
 <code>FLASK_ENV=development</code>
 <code>flask run</code>
--- app.py
+++ app.py
@@ -11,3 +11,15 @@ def hello():
 @app.route('/about/')
 def about():
     return render_template('about.html')
+
+# ...
+
+@app.route('/comments/')
+def comments():
+    comments = ['This is the first comment.',
+                'This is the second comment.',
+                'This is the third comment.',
+                'This is the fourth comment.'
+                ]
+
+    return render_template('comments.html', comments=comments)
